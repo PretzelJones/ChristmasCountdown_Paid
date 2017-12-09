14c7759638198e83360bead4d4b2419e3764f615
Sean Patterson <sean@bosson.design>
2017-12-09 18:55:11 -0500

    initial commit

---
+++ app/src/main/java/com/bossondesign/christmascountdown/MainActivity.java
@@ -0,0 +1,120 @@
+package com.bossondesign.christmascountdown;
+
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView txtTimerDay;
+    private TextView tvEvent;
+    private Handler handler;
+    private Runnable runnable;
+    MediaPlayer mediaPlayer;
+    int length;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtTimerDay = findViewById(R.id.txtTimerDay);
+        countDownStart();
+
+        new AppRater(this).show();
+
+        super.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        /*
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.animate);
+
+        ImageView imageAnimation;
+        imageAnimation = findViewById(R.id.imageView);
+        imageAnimation.startAnimation(animation);
+        animation.setRepeatCount(Animation.INFINITE);
+        */
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.song);
+        mediaPlayer.start();
+        mediaPlayer.setLooping(true);
+    }
+
+    public void countDownStart() {
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                handler.postDelayed(this, 1000);
+                try {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat(
+                            "yyyy-MM-dd");
+// Please here set your event date//YYYY-MM-DD
+                    Date futureDate = dateFormat.parse("2017-12-26");
+                    Date currentDate = new Date();
+                    if (!currentDate.after(futureDate)) {
+                        long diff = futureDate.getTime()
+                                - currentDate.getTime();
+                        long days = diff / (24 * 60 * 60 * 1000);
+                        diff -= days * (24 * 60 * 60 * 1000);
+                        long hours = diff / (60 * 60 * 1000);
+                        diff -= hours * (60 * 60 * 1000);
+                        long minutes = diff / (60 * 1000);
+                        diff -= minutes * (60 * 1000);
+                        long seconds = diff / 1000;
+                        txtTimerDay.setText("" + String.format("%02d", days));
+                        /*
+                        txtTimerHour.setText("" + String.format("%02d", hours));
+                        txtTimerMinute.setText(""
+                                + String.format("%02d", minutes));
+                        txtTimerSecond.setText(""
+                                + String.format("%02d", seconds));
+                                */
+                    } else {
+                        tvEvent.setVisibility(View.VISIBLE);
+                        tvEvent.setText("The event started!");
+                        textViewGone();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        handler.postDelayed(runnable, 1 * 1000);
+    }
+
+    public void textViewGone() {
+        findViewById(R.id.textView1).setVisibility(View.GONE);
+    }
+
+    //prevent reload on orientation change - retain state on shift
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+        mediaPlayer.pause();
+        length = mediaPlayer.getCurrentPosition();
+    }
+
+    @Override
+    protected void onRestart() {
+
+        super.onRestart();
+        mediaPlayer.start();
+        mediaPlayer.seekTo(length);
+    }
+
+}
